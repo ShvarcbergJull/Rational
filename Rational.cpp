df4b1b952cadd75aaf7da6479b4660221c2bbc17
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2018-10-15 20:48:08 +0800

    Update Rational.cpp

--- Rational.cpp
+++ Rational.cpp
@@ -62,6 +62,13 @@ Rational Rational::operator +(const Rational& r) const
 	return res += r;
 }
 
+Rational Rational::operator +(const int& x) const
+{
+	Rational r(x);
+	
+	return (*this + r);
+}
+
 Rational Rational::operator *(const Rational& r)
 {
 	Rational res(*this);
@@ -76,6 +83,28 @@ Rational Rational::operator *(const int& n)
 	simplify();
 	return res;
 }
+/*
+Rational Rational::operator *(const double& n)
+{
+	Rational res(*this);
+	Rational d;
+	double f = n;
+	int p = f, k = 1;
+	f -= p;
+	while (f > 0 && k < 1000)
+	{
+		f *= 10;
+		int i = f;
+		d.numer = d.numer * 10 + i;
+		f -= i;
+		k *= 10;
+	}
+	d.denom = k;
+	d.numer += p * k;
+	d.simplify();
+	d = res * d;
+	return d;
+}*/
 
 Rational& Rational::operator /(const Rational& r)
 {
@@ -101,7 +130,9 @@ Rational Rational::operator -() const
 
 Rational& Rational::operator -(const Rational& r)
 {
-	return (*this += (-r));
+	Rational res(*this);
+
+	return (res += (-r));
 }
 
 Rational& Rational::operator ++()
@@ -122,7 +153,20 @@ bool Rational::operator ==(const Rational& r) const
 	return (numer == r.numer) && (denom == r.denom);
 }
 
+bool Rational::operator ==(const int& r) const
+{
+	Rational res(*this), r1(r);
+	r1.denom *= res.denom;
+	r1.numer *= res.denom;
+	return (res == r1);
+}
+
 bool Rational::operator !=(const Rational& r) const
+{
+	return !(*this == r);
+}
+
+bool Rational::operator !=(const int& r) const
 {
 	return !(*this == r);
 }
@@ -150,7 +194,7 @@ Rational::operator double() const
 {
 	return ((double)numer / denom);
 }
-
+/*
 Rational Rational::srt()
 {
 	Rational res(*this);
@@ -189,6 +233,62 @@ Rational Rational::srt()
 
 	res = n / d;
 	res.simplify();
+
+	return res;
+}*/
+
+Rational Rational::srt()
+{
+	Rational res(*this), x1, y1;
+	double x = 2, y = 2;
+
+	while (res.numer != (int(x * x * 10000)/10000.0))
+	{
+		x = ((res.numer / x) + x) * 1 / 2;
+	}
+	x = int(x * 1000) / 1000.0;
+
+	while (res.denom != (int(y * y * 10000) / 10000.0))
+	{
+		y = ((res.denom / y) + y) * 1 / 2;
+	}
+	y = int(y * 1000) / 1000.0;
+
+	
+	for (int i = 0; i < 2; i++)// для чисел из которых ровно не извлекается корень
+	{
+		Rational n;
+		double f;
+		if (i == 0)
+			f = x;
+		else
+			f = y;
+
+		int p = f, k = 1;
+		f -= p;
+		while (f > 0 && k < 1000)
+		{
+			f *= 10;
+			int i = f;
+			n.numer = n.numer * 10 + i;
+			f -= i;
+			k *= 10;
+		}
+		
+		n.denom = k;
+		n.numer += p * k;
+
+		if (i == 0)
+			x1 = n;
+		else
+			y1 = n;
+	}
+
+	res = x1 / y1;
+	simplify();
+
+	/*res.numer = x;
+	res.denom = y;*/
 
 	return res;
 }
